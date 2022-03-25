e55a6b0545521ea6da7544976d58c1832bbb5dce
Zülal Nebin <zllnbn1997@gmail.com>
2022-03-25 12:14:14 +0300

    format code

--- client/src/pages/Admin/Home/index.js
+++ client/src/pages/Admin/Home/index.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
-    return (
-        <div>
-            Admin Home
-        </div>
-    )
+  return <div>Admin Home</div>;
 }
 
-export default Home
+export default Home;
--- client/src/pages/Auth/Signup/index.js
+++ client/src/pages/Auth/Signup/index.js
@@ -1,115 +1,110 @@
-import React from 'react';
+import React from "react";
 import {
-    Flex,
-    Box,
-    Heading,
-    FormControl,
-    FormLabel,
-    Input,
-    Button,
-    Alert
-} from '@chakra-ui/react';
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Alert,
+} from "@chakra-ui/react";
 
-import { useFormik } from 'formik';
-import validationSchema from './validations';
+import { useFormik } from "formik";
+import validationSchema from "./validations";
 
-import { fetchRegister } from '../../../api';
+import { fetchRegister } from "../../../api";
 
-import { useAuth } from '../../../contexts/AuthContext';
+import { useAuth } from "../../../contexts/AuthContext";
 
 function Signup({ history }) {
-    const { login } = useAuth();
+  const { login } = useAuth();
 
-    const formik = useFormik({
-        initialValues: {
-            email: "",
-            password: "",
-            passwordConfirm: "",
-        },
-        validationSchema,
-        onSubmit: async (values, bag) => {
-            try {
-                const registerResponse = await fetchRegister({
-                    email: values.email,
-                    password: values.password
-                });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+    validationSchema,
+    onSubmit: async (values, bag) => {
+      try {
+        const registerResponse = await fetchRegister({
+          email: values.email,
+          password: values.password,
+        });
 
-                login(registerResponse);
+        login(registerResponse);
 
-                history.push("/profile");
-                console.log(registerResponse);
-            } catch (e) {
-                bag.setErrors({ general: e.response.data.message })
-            }
-        },
-    });
+        history.push("/profile");
+        console.log(registerResponse);
+      } catch (e) {
+        bag.setErrors({ general: e.response.data.message });
+      }
+    },
+  });
 
-    return (
-        <div>
-            <Flex align="center" width="full" justifyContent="center">
-                <Box pt={10} >
-                    <Box textAlign="center">
-                        <Heading>Sign Up</Heading>
-                    </Box>
-                    <Box my={5} >
-                        {
-                            formik.errors.general && (
-                                <Alert status="error">
-                                    {formik.errors.general}
-                                </Alert>
-                            )
-                        }
-                    </Box>
-                    <Box my={5} textAlign="left">
-                        <form onSubmit={formik.handleSubmit}>
-                            <FormControl>
-                                <FormLabel>E-mail</FormLabel>
-                                <Input
-                                    name="email"
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                    value={formik.values.email}
-                                    isInvalid={formik.touched.email && formik.errors.email}
-                                />
-                            </FormControl>
+  return (
+    <div>
+      <Flex align="center" width="full" justifyContent="center">
+        <Box pt={10}>
+          <Box textAlign="center">
+            <Heading>Sign Up</Heading>
+          </Box>
+          <Box my={5}>
+            {formik.errors.general && (
+              <Alert status="error">{formik.errors.general}</Alert>
+            )}
+          </Box>
+          <Box my={5} textAlign="left">
+            <form onSubmit={formik.handleSubmit}>
+              <FormControl>
+                <FormLabel>E-mail</FormLabel>
+                <Input
+                  name="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  isInvalid={formik.touched.email && formik.errors.email}
+                />
+              </FormControl>
 
-                            <FormControl mt="4" >
-                                <FormLabel>Password</FormLabel>
-                                <Input
-                                    name="password"
-                                    type="password"
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                    value={formik.values.password}
-                                    isInvalid={
-                                        formik.touched.password &&
-                                        formik.errors.password
-                                    }
-                                />
-                            </FormControl>
+              <FormControl mt="4">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                  isInvalid={formik.touched.password && formik.errors.password}
+                />
+              </FormControl>
 
-                            <FormControl mt="4" >
-                                <FormLabel>Password Confirm</FormLabel>
-                                <Input
-                                    name="passwordConfirm"
-                                    type="password"
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                    value={formik.values.passwordConfirm}
-                                    isInvalid={formik.touched.passwordConfirm && formik.errors.passwordConfirm}
+              <FormControl mt="4">
+                <FormLabel>Password Confirm</FormLabel>
+                <Input
+                  name="passwordConfirm"
+                  type="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.passwordConfirm}
+                  isInvalid={
+                    formik.touched.passwordConfirm &&
+                    formik.errors.passwordConfirm
+                  }
+                />
+              </FormControl>
 
-                                />
-                            </FormControl>
-
-                            <Button mt="4" width="full" type="submit">
-                                Sign Up
-                            </Button>
-                        </form>
-                    </Box>
-                </Box>
-            </Flex>
-        </div>
-    );
+              <Button mt="4" width="full" type="submit">
+                Sign Up
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
